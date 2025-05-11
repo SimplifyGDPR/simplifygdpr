ea578dd5e88692e92413f6befcd265a968444982
SimplifyGDPR <211221578+SimplifyGDPR@users.noreply.github.com>
2025-05-11 16:05:32 +0200

    Incluido router de usuarios autorizados en la API

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
-from backend.routers import legal
+from backend.routers import legal, usuarios
 
 app = FastAPI()
 
 app.include_router(legal.router)
+app.include_router(usuarios.router)
 
 @app.get("/ping")
 def ping():
