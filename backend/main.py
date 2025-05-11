1a220440529aa6d694d38b57de967bc4ba4aad25
SimplifyGDPR <211221578+SimplifyGDPR@users.noreply.github.com>
2025-05-11 16:21:53 +0200

    Incluido router de departamentos en la API

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from backend.routers import legal, usuarios
+from backend.routers import legal, usuarios, departamentos
 
 app = FastAPI()
 
 app.include_router(legal.router)
 app.include_router(usuarios.router)
+app.include_router(departamentos.router)
 
 @app.get("/ping")
 def ping():
