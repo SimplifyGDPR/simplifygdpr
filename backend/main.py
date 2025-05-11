26838b623b313aa34bada59acdb8a7dda78a4ae1
SimplifyGDPR <211221578+SimplifyGDPR@users.noreply.github.com>
2025-05-11 17:28:02 +0200

    Añadido router /panel a la API principal

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from backend.routers import legal, usuarios, departamentos, politicas, auth
+from backend.routers import legal, usuarios, departamentos, politicas, auth, panel
 
 app = FastAPI()
 
@@ -8,6 +8,7 @@ app.include_router(usuarios.router)
 app.include_router(departamentos.router)
 app.include_router(politicas.router)
 app.include_router(auth.router)
+app.include_router(panel.router)
 
 @app.get("/ping")
 def ping():
