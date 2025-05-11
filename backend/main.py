112167cd6757df817dd6e688d67e76974364b0f2
SimplifyGDPR <211221578+SimplifyGDPR@users.noreply.github.com>
2025-05-11 16:33:11 +0200

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from backend.routers import legal, usuarios, departamentos, politicas
+from backend.routers import legal, usuarios, departamentos, politicas, auth
 
 app = FastAPI()
 
@@ -7,6 +7,7 @@ app.include_router(legal.router)
 app.include_router(usuarios.router)
 app.include_router(departamentos.router)
 app.include_router(politicas.router)
+app.include_router(auth.router)
 
 @app.get("/ping")
 def ping():
