e5cad0727992df2d402eaa947f478c3c29a35950
SimplifyGDPR <211221578+SimplifyGDPR@users.noreply.github.com>
2025-05-11 16:28:27 +0200

    Incluido router de políticas legales en la API

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from backend.routers import legal, usuarios, departamentos
+from backend.routers import legal, usuarios, departamentos, politicas
 
 app = FastAPI()
 
 app.include_router(legal.router)
 app.include_router(usuarios.router)
 app.include_router(departamentos.router)
+app.include_router(politicas.router)
 
 @app.get("/ping")
 def ping():
